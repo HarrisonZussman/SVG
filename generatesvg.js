0dfa2bbf320a8993d96ec65dbc81cdb7a0228fc9
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-03-30 22:29:35 -0400

    get base locic working

--- generatesvg.js
+++ generatesvg.js
@@ -1,15 +1,20 @@
 const {Square, Circle, Triangle} = require("./lib/shape.js");
+const CreateSvg = require("./lib/svg.js")
 
 function generatesvg(userResponse){
-    var color = `'${userResponse.shapeColor}'`
-    var shape
-    if (userResponse.shapeLogo=="Square"){
-        shape = new Square(color)}
-        else if (userResponse.shapeLogo=="Circle"){
-            shape = new Circle(color)}
-            else if (userResponse.shapeLogo=="Triangle"){
-                shape = new Triangle(color)
+    var color = userResponse.shapeColor
+    var shape;
+    if (userResponse.shape=="Square"){
+        shape = new Square()}
+        else if (userResponse.shape=="Circle"){
+            shape = new Circle()}
+            else if (userResponse.shape=="Triangle"){
+                shape = new Triangle()
     }
+    shape.setColor(color)
+    const svg = new CreateSvg()
+    svg.setText(userResponse.text,userResponse.textColor)
+    svg.setShape(shape)
     return`
     <svg version="1.1"
     width="300" height="200"
--- index.js
+++ index.js
@@ -10,30 +10,32 @@ inquirer.prompt(
 {
     type: 'input',
     message: 'Shape color?',
-    name: 'instructions',
+    name: 'shapeColor',
     validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } },
 },
 {
     type: 'input',
     message: 'Text color?',
-    name: 'credit',
+    name: 'textColor',
     validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } },
 },
 {
     type: 'input',
     message: '3 letter text?',
-    name: 'usage',
+    name: 'text',
     validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } },
 },
 {
     type: 'list',
     message: 'shape?',
-    name: 'license',
+    name: 'shape',
     choices: ['Square','Triangle', 'Circle'],
     validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } },
 }
 ]).then(
     function createNewFile(data){
+        console.log(data);
+    
         generatesvg(data)
     //     console.log(data);
     //     fs.writeFile(`./logo.svg`,data,(err)=>{
@@ -44,24 +46,3 @@ inquirer.prompt(
     // })
     }
 )
-class CreateSvg{
-    construct(){
-        this.textEl = ""
-        this. shapeEl = ""
-    }
-    render(){
-        console.log(`<svg version="1.1"
-        width="300" height="200"
-        xmlns="http://www.w3.org/2000/svg">
-        setText())
-        ${this.textEl}
-        ${this.shapeEl}
-        /svg>`);
-    }
-    setText(text,color){
-        this.textEl = `<text x="150" y="125" font-size="60" text-`
-    }
-    setShape(shape,color){
-        this.shapeEl = shape.render()
-    }
-}
